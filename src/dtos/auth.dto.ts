1705be220e1e4d5bc21786c4ebd2a5513d9778f5
ducnv2509 <ducit2509@gmail.com>
2022-11-21 19:05:47 +0700

    aa

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const document = Joi.string().regex(new RegExp('^[1-9]{1}[0-9]{5,9}$')).min(6).max(10);
+const document = Joi.string().regex(new RegExp('^[0-9]{1}[0-9]{5,9}$')).min(6).max(10);
 
 const password = Joi.string().regex(new RegExp('^[\\w0-9]+$')).min(8).max(16);
 
