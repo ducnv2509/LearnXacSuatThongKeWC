ca2c379d7f094ea27ad51b42e5efcc0e0f088ec6
ducnv2509 <ducit2509@gmail.com>
2022-11-22 10:43:52 +0700

    asdads

--- src/schemas/userMatches.schema.ts
+++ src/schemas/userMatches.schema.ts
@@ -3,8 +3,8 @@ import { Schema, Types } from 'mongoose';
 export const userMatchesSchema = new Schema({
   _id: {
     type: Types.ObjectId,
-    required:true,
-    auto: true 
+    required: true,
+    auto: true
   },
   user_id: {
     type: String,
@@ -19,7 +19,37 @@ export const userMatchesSchema = new Schema({
   },
   visitor_score: {
     type: Number,
-  }
+  },
+  bet: [{
+
+    scoreBet: {
+      localBet: {
+        type: Number,
+      },
+      visitorBet: {
+        type: Number,
+      },
+      betAmount: {
+        type: Number,
+      }
+    },
+    winBet: {
+      localBet: {
+        type: Boolean,
+      },
+      visitorBet: {
+        type: Boolean,
+
+      },
+      tieBet: {
+        type: Boolean,
+      },
+      betAmount: {
+        type: Number,
+      }
+    }
+
+  }]
 }, {
   collection: 'users_matches',
   versionKey: false
